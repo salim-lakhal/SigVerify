de7f8be589a4196c75c3f69eab7fb3b0b90ba94f
Wscheeee <scherrer.wilfried@.com>
2024-06-01 16:59:39 +0200

    fix

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -3,28 +3,29 @@ import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import styles from './navbar.module.css';
 import { SignOutButton } from '@clerk/nextjs';
+import type { MouseEvent, KeyboardEvent } from 'react';
 
 function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
     const router = useRouter();
-    const menuRef = useRef(null);
-    const menuIconRef = useRef(null);
+    const menuRef = useRef<HTMLDivElement>(null);
+    const menuIconRef = useRef<HTMLDivElement>(null);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
-    const handleClickOutside = (event) => {
+    const handleClickOutside = (event: MouseEvent | Event) => {
         if (
-            menuRef.current && !menuRef.current.contains(event.target) &&
-            menuIconRef.current && !menuIconRef.current.contains(event.target)
+            menuRef.current && !menuRef.current.contains(event.target as Node) &&
+            menuIconRef.current && !menuIconRef.current.contains(event.target as Node)
         ) {
             setIsOpen(false);
         }
     };
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Escape') {
+    const handleKeyDown = (event: KeyboardEvent | Event) => {
+        if (event instanceof KeyboardEvent && event.key === 'Escape') {
             setIsOpen(false);
         }
     };
@@ -43,7 +44,7 @@ function NavBar() {
             document.removeEventListener('keydown', handleKeyDown);
         };
     }, [isOpen]);
-    
+
     const navigateToDashboard = () => {
         router.push('/dashboard');
     };
@@ -63,7 +64,7 @@ function NavBar() {
         <div className={styles.header}>
             <div className={styles.logoContainer}>
                 <div className={styles.logoWrapper}>
-                    <Image src="/contract.png" alt="Logo" className={styles.logo} fill />
+                    <Image src="/contract.png" alt="Logo" className={styles.logo} layout="fill" />
                 </div>
                 <h2 className={styles.sigtitle}>SigVerify</h2>
             </div>
@@ -73,7 +74,7 @@ function NavBar() {
                 <div className={styles.burger}></div>
             </div>
             <div ref={menuRef} className={`${styles.navButtons} ${isOpen ? styles.showMenu : ''}`}>
-            <button className={styles.navButton} onClick={navigateToDashboard}>Dashboard</button>
+                <button className={styles.navButton} onClick={navigateToDashboard}>Dashboard</button>
                 <button className={styles.navButton} onClick={navigateToNewDoc}>New Document</button>
                 <button className={styles.navButton} onClick={navigateToManageDocs}>Manage Documents</button>
                 <button className={styles.navButton} onClick={navigateToProfile}>Profile</button>
